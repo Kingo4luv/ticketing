5fbc19a43a92405e8dcea6b6eee06ecf366c3ff1
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-07-24 23:44:40 +0100

    Added mongodb and created a model

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 const router = express.Router();
 
 router.post(
@@ -13,16 +14,27 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
-    const { email, password } = req.body;
-    console.log("Creating a user");
 
-    throw new DatabaseConnectionError();
-    res.send({});
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      console.log("Email in Use");
+      return res.send({});
+    }
+
+    const user = User.build({
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
