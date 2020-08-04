f8ddbe557be7b65656509551f88c2ce1cd7a8004
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-08-04 22:19:08 +0100

    server rendering get data

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,9 +5,19 @@ const Index = ({ currentUser }) => {
   return <h1> Index </h1>;
 };
 
-Index.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
-  return response.data;
+Index.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
 };
 
 export default Index;
