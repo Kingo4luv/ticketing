749008eec30d267df9d7a40057ed9c4da6b6b59e
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-08-05 20:54:32 +0100

    working with tickets service

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,59 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../app";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[];
+    }
+  }
+}
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "asdfg";
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const email = "test@test.com";
+  const id = "12bs529ks82";
+
+  const payload = {
+    id,
+    email,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`express:sess=${base64}`];
+};
