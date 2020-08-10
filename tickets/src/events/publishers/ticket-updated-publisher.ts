0c5c55ee5e88f13e23ba792c2801c03ffc06c205
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-08-10 23:29:14 +0100

    Created ticket publisher and listeners

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, TicketUpdatedEvent, Subjects } from "@keatickets/common";
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
