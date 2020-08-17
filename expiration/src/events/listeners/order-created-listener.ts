01b4f258b011b0d8d2d8ddbdf2f6e9397a2f9ea1
Kingsley Atanang <Kingo4luv@yahoo.com>
2020-08-17 15:14:44 +0100

    completed worker service

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,10 +1,23 @@
 import { Message } from "node-nats-streaming";
 import { Listener, OrderCreatedEvent, Subjects } from "@keatickets/common";
 import { queueGroupName } from "./queueGroupName";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent["data"], msg: Message);
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("Waiting this many milliseconds to process the Job:", delay);
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay: delay,
+      }
+    );
+    msg.ack();
+  }
 }
